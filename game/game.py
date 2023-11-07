e58248f3fea6d6de74178bb596424b1d6a80b6bd
chitwitgit <100676229+chitwitgit@users.noreply.github.com>
2023-11-07 12:04:26 +0800

    updated game.py

--- game/game.py
+++ game/game.py
@@ -15,8 +15,27 @@ class Sprite(ABC):
         pass
 
     @abstractmethod
-    def render(self):
+    def render(self, win):
         pass
+
+
+class Ball(Sprite):
+    def __init__(self, x, y, radius, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+
+    def _move(self):
+        # Move the ball randomly up or down
+        direction = random.choice([-1, 1])
+        self.y += direction * 5
+
+    def update(self):
+        self._move()
+
+    def render(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
 
 class Game:
@@ -32,13 +51,15 @@ class Game:
         self.points = 0
         self.currentScene = 0
 
+        self.ball = Ball(250, 400, 50, (255, 0, 0))
+
     def reset(self, seed=None, options=None):
         random.seed(seed)
         return
 
     def step(self, action):
         self.steps += 1
-        return
+        self.ball.update()
 
     def render(self):
         return self._render_frame(False)
@@ -48,19 +69,20 @@ class Game:
             pygame.init()
             pygame.display.init()
             self.window = pygame.display.set_mode((self.screen_width, self.screen_height))
-            mixer.music.load("")
-            mixer.music.set_volume(0.8)
-            mixer.music.play(-1)
+            # mixer.music.load("")
+            # mixer.music.set_volume(0.8)
+            # mixer.music.play(-1)
         if self.clock is None:
             self.clock = pygame.time.Clock()
         if terminated:
             _ = 1  # do something
         win = pygame.Surface((self.screen_width, self.screen_height))
-        background = pygame.image.load("").convert_alpha()
-        background = pygame.transform.scale(background, (self.screen_width, self.screen_height))
-        win.blit(background, (0, 0))
+        # background = pygame.image.load("").convert_alpha()
+        # background = pygame.transform.scale(background, (self.screen_width, self.screen_height))
+        # win.blit(background, (0, 0))
+        win.fill((0, 0, 0))  # Fill the surface with black color
         # rendering objects
-
+        self.ball.render(win)
         # render window buffer to screen
         self.window.blit(win, win.get_rect())
         pygame.event.pump()
@@ -71,3 +93,22 @@ class Game:
         if self.window is not None:
             pygame.display.quit()
             pygame.quit()
+
+
+def main():
+    game = Game()
+    game.render()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        game.step(0)
+        game.render()
+
+    game.close()
+
+
+if __name__ == '__main__':
+    main()
