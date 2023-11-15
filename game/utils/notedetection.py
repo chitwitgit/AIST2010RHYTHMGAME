801fcb4ba09ee4dc47ffb26b5480a8278f21ee89
TangWaiLun-Brian <1155174601@link.cuhk.edu.hk>
2023-11-16 00:23:38 +0800

    adjust parameters for note padding

--- game/utils/notedetection.py
+++ game/utils/notedetection.py
@@ -9,7 +9,43 @@ def load_audio(filename):
 def onset_roundings(onset_times, onset_durations, tempo, precision=0.25):
     spb = 60 / tempo * precision
     onset_times = np.around(onset_times / spb, decimals=0) * spb
+    onset_durations = np.around(onset_durations / spb, decimals=0) * spb
     onset_durations = np.clip(onset_durations, spb, 99999)
+    return onset_times, onset_durations
+
+
+def onset_paddings(onset_times, onset_durations, tempo, abs_x, precisions=1.0, sr=22050):
+    spb = 60 / tempo * precisions
+    local_range = 100
+    onset_samples = librosa.time_to_samples(onset_times, sr=sr)
+
+    length = len(onset_times)
+    i = 0
+    while i < length-1:
+        interval_samples = np.arange(-local_range, local_range).reshape(1, -1) + onset_samples[i]
+        pre_amplitude = abs_x[interval_samples].reshape(-1, 2 * local_range)
+        pre_amplitude = np.max(pre_amplitude, axis=1)
+        count = 0
+        # print(i, onset_times.shape, length)
+        while onset_times[i+1] - onset_times[i] - onset_durations[i] - count * spb > spb:
+            new_start_time = onset_times[i] + onset_durations[i] + count * spb
+            new_onset_sample = librosa.time_to_samples(new_start_time, sr=sr)
+
+            interval_samples = np.arange(-local_range, local_range).reshape(1, -1) + new_onset_sample
+            cur_amplitude = abs_x[interval_samples].reshape(-1, 2 * local_range)
+            cur_amplitude = np.max(cur_amplitude, axis=1)
+
+            if cur_amplitude >= pre_amplitude / 2:
+                i += 1
+                length += 1
+                onset_times = np.insert(onset_times, i, new_start_time, axis=0)
+                onset_durations = np.insert(onset_durations, i, spb, axis=0)
+                onset_samples = np.insert(onset_samples, i, new_onset_sample, axis=0)
+                count = 0
+            else:
+                count += 1
+
+        i += 1
     return onset_times, onset_durations
 
 def onset_detection(x, fs, fft_length=1024, fft_hop_length=512):
@@ -22,14 +58,15 @@ def onset_detection(x, fs, fft_length=1024, fft_hop_length=512):
     # using onset_detect from librosa to detect onsets (using parameters delta=0.04, wait=4)
     onset_times = librosa.frames_to_time(onset_frames, sr=fs)
     onset_samples = librosa.frames_to_samples(onset_frames)
-    onset_durations = onset_length_detection(x, y, onset_samples)
+    onset_durations = onset_length_detection(x, y, onset_samples, sr=fs)
 
     onset_times, onset_durations = onset_roundings(onset_times, onset_durations, tempo)
+    onset_times, onset_durations = onset_paddings(onset_times, onset_durations, tempo, np.abs(x), sr=fs)
     return onset_times, onset_durations
 
 
 # detect length of each onsets
-def onset_length_detection(x, y, onset_samples, fft_length=1024, fft_hop_length=512, sr=22050, tolerance=2):
+def onset_length_detection(x, y, onset_samples, fft_length=1024, fft_hop_length=512, sr=22050, tolerance=3):
     residual_size = fft_length - fft_hop_length
     filtered_onset_samples = onset_samples
     filtered_onset_samples[onset_samples < fft_length] = fft_length - residual_size
@@ -72,7 +109,7 @@ def onset_length_detection(x, y, onset_samples, fft_length=1024, fft_hop_length=
 
         # compute distribution difference
         # satisfaction = np.logical_and(satisfaction, diff > 0.5)
-        satisfaction = np.logical_and(satisfaction, diff < 0.5)
+        satisfaction = np.logical_and(satisfaction, diff < 1.2)
 
 
         # check change in max frequency peak
@@ -86,7 +123,7 @@ def onset_length_detection(x, y, onset_samples, fft_length=1024, fft_hop_length=
         cur_amplitude = abs_x[interval_samples].reshape(-1, 2 * local_range)
         cur_amplitude = np.max(cur_amplitude, axis=1)
         mask2 = (standard_amplitude - cur_amplitude <= standard_amplitude * 0.8)
-        satisfaction = np.logical_and(satisfaction, mask2)
+        # satisfaction = np.logical_and(satisfaction, mask2)
 
 
         valid_mask = np.logical_and(valid_mask, satisfaction)
