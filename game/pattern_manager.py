90ddeb8fbe28f61c06da26c2ba42ce89ba686115
stephentwk <stephen.ptwk@gmail.com>
2023-12-15 22:53:49 +0800

    commented pattern_manager.py

--- game/pattern_manager.py
+++ game/pattern_manager.py
@@ -31,22 +31,31 @@ class PatternManager:
         self.last_onset_time = 0
         self.last_circle_position = np.array([self.screen_width / 2, self.screen_height / 2])
 
-        # game boundaries
+        # game boundaries for where the circle objects can be placed on the screen
         self.x_range = (self.radius * 5, screen_width - self.radius * 5)
         self.y_range = (self.radius * 2, self.screen_height - self.radius * 2)
 
     def generate_map(self, music_data):
+        '''
+        Generate objects/patterns and store in self.pattern_queue
+        :param music_data: contains the timings, durations and bar numbers of the onsets
+        :return: nothing
+        '''
         onset_times, onset_durations, onset_bars, *_ = music_data
         onset_time_frames = [int(i * self.fps) for i in onset_times]
         onset_duration_frames = [int(i * self.fps) for i in onset_durations]
-        # print(onset_time_frames)
-        # print(onset_duration_frames)
-        # print(onset_bars)
         self.generate_patterns(onset_time_frames, onset_duration_frames, onset_bars)
         self.pattern_queue = self.patterns[:self.queue_length]
         return
 
     def generate_patterns(self, onset_time_frames, onset_duration_frames, onset_bars):
+        '''
+        Determines the objects' locations on the screen according to their bar number and onset timings
+        :param onset_time_frames: list of onset timings in frame number
+        :param onset_duration_frames: list of  onset durations in frames
+        :param onset_bars: list of bar numbers of all the onsets
+        :return: nothing
+        '''
         # make the seed dependent on the input audio in some way
         seed_add = sum(onset_duration_frames)
         random.seed(self.seed + seed_add)
@@ -58,9 +67,6 @@ class PatternManager:
         grouped_data = [list(group) for key, group in groupby(zipped_data, key=lambda x: x[2])]
         onset_time_frames = [[item[0] for item in group] for group in grouped_data]
         onset_duration_frames = [[item[1] for item in group] for group in grouped_data]
-        # print(onset_time_frames)
-        # print(onset_duration_frames)
-        # print(onset_bars)
 
         # distances for each circle of a pattern and for each pattern depends on the difficulty
         circle_distance = 20 + 50 * self.difficulty
@@ -79,9 +85,11 @@ class PatternManager:
                                         np.linalg.norm(bottom_left_corner - self.last_circle_position),
                                         np.linalg.norm(top_left_corner - self.last_circle_position))
             if max_possible_distance < pattern_distance:
+                # which means there is no way to find the next object with the predetermined pattern distance
+                # within the playable boundary
                 while circle_position is None or np.linalg.norm(
                         circle_position - self.last_circle_position) != min_circle_distance:
-                    # Generate a random angle in radians
+                    # Generate a random angle in radians, repeat until a suitable direction is found
                     angle = np.random.uniform(0, 2 * np.pi)
 
                     # Calculate the coordinates for the circle_position
@@ -155,22 +163,35 @@ class PatternManager:
                             break
 
     def generate_object(self, onset_time, onset_duration, circle_position):
+        '''
+        Determine object type according to the onset duration and add to the pattern queue
+        :param onset_time: onset timing for one note
+        :param onset_duration: onset duration for one note
+        :param circle_position: the note's circle position on the screen from generate_patterns
+        :return: nothing
+        '''
+        # a little buffering in case detected onsets are too close to each other
         if onset_time - self.last_onset_time < 10:
             return
         self.last_onset_time = onset_time
         if onset_duration <= self.beat_duration * self.fps * 4:
+            # choose circle object as the pattern type if the onset duration is short enough
             pattern_type = "TapPattern"
             t = onset_time
             starting_t = t
             ending_t = t + self.beat_duration * self.fps
         else:
+            # choose slider object if the onset duration is long enough
             pattern_type = random.choice(["Line", "CubicBezier", "Arc"])
             t = onset_time
             starting_t = t
             ending_t = t + onset_duration / 16
             length = 100 / (self.beat_duration * self.fps) * onset_duration / 8
 
+        # randomize circle color
         color = (random.randint(150, 255), random.randint(150, 255), random.randint(150, 255))
+
+        # add the pattern object to the pattern queue according to the previously determined object type
         if pattern_type == "TapPattern":
             position = circle_position
             tap = TapPattern(position, self.radius, self.stroke_width, color, t, self.lifetime, self.approach_rate)
