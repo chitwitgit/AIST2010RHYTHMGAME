bba104397146d8c35a79cc0235e998ab4764a6bf
chitwitgit <100676229+chitwitgit@users.noreply.github.com>
2023-12-16 16:20:51 +0800

    Updated help command in cmdargs.py

--- game/utils/cmdargs.py
+++ game/utils/cmdargs.py
@@ -4,9 +4,6 @@ parser = argparse.ArgumentParser(
     formatter_class=argparse.RawTextHelpFormatter,
     description="Pygame rhythm game app with automatic map generation."
 )
-parser.add_argument('-m', '--mode', type=str, help="Mode (leave blank for default mode)",
-                    choices=['playing', 'testing', 'debug'],
-                    default='playing')
 parser.add_argument('-s', "--seed", type=int,
                     help="Seed for random number generator",
                     default=None)
@@ -24,8 +21,7 @@ parser.add_argument('-y', "--youtube", type=str,
                     default=None)
 
 parser.epilog = """Example usage:
-  python main.py -m playing -s 123 -t 120 -y "idk"
-  python main.py --mode testing --tempo 140 -y "https://www.youtube.com/watch?v=example"
+  python main.py -d 6 -a 10 --tempo 246 -y "https://www.youtube.com/watch?v=-LwBbLa_Vhc"
 """
 
 args = parser.parse_args()
