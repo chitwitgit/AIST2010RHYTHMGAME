a07419bc3fb99a337034cff4349bb20d33c1299c
stephentwk <stephen.ptwk@gmail.com>
2023-12-16 16:05:50 +0800

    added demonstration video link and its correct tempo

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from game.utils.cmdargs import args
 
 def main():
     # Manage command line arguments
-    youtube_link = args.youtube if args.youtube is not None else "https://www.youtube.com/watch?v=_5aZ6bhho68"
+    youtube_link = args.youtube if args.youtube is not None else "https://www.youtube.com/watch?v=-LwBbLa_Vhc"
     seed = args.seed if args.seed is not None else 777
     given_tempo = args.tempo if args.tempo is not None else None
+    if youtube_link == "https://www.youtube.com/watch?v=-LwBbLa_Vhc":   # for demonstration purposes
+        given_tempo = 246
     difficulty = args.difficulty if args.difficulty is not None else 5
     approach_rate = args.ar if args.ar is not None else 10
 
